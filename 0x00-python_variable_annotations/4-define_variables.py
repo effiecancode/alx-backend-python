1af5b41a2a5f6d888997bba79eb78ea5f3e2adeb
effiecancode <faitheffie25@gmail.com>
2024-03-14 22:50:51 +0300

    define variables

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Define and annotate variables with specifies values
+"""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
