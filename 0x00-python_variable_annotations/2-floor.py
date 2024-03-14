d9bd9cfa223187638a359f9b5999447e60737553
effiecancode <faitheffie25@gmail.com>
2024-03-14 22:47:16 +0300

    math.floor method

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Typed-annotations
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """
+    function that calculates the floor of an
+    int
+    """
+    return math.floor(n)
