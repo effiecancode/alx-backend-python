6436ed811485301a5b5af63c59fced77a440cad7
effiecancode <faitheffie25@gmail.com>
2024-03-14 22:46:05 +0300

    annotated concat function

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Type-annotated function
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Type annotated function that returns a str
+    """
+    return str1 + str2
